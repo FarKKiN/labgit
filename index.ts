e696b2944f6380e8ab8df2459791884309e02566
FarKKiN <farkk77@mail.ru>
2026-09-22 14:56:10 +0300

    Инициализация проекта и меню

---
+++ index.ts
@@ -0,0 +1,13 @@
+function showMenu() {
+    console.log("Меню");
+    console.log("1. Ввести два числа");
+    console.log("2. Выполнить сложение");
+    console.log("3. Выполнить вычитание");
+    console.log("4. Выполнить деление");
+    console.log("5. Возвести число в степень");
+}
+
+let num1: number = 0;
+let num2: number = 0;
+
+showMenu();
