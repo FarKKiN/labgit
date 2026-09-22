666635d44d9077588c4eeb3c1a04c921dc541f7f
FarKKiN <farkk77@mail.ru>
2026-09-22 15:18:04 +0300

    функция деления

--- index.ts
+++ index.ts
@@ -11,3 +11,11 @@ let num1: number = 0;
 let num2: number = 0;
 
 showMenu();
+
+function divide(a: number, b: number): number {
+    if (b === 0) {
+        console.log("делить на ноль нельзя");
+        return 0;
+    }
+    return a / b;
+}
